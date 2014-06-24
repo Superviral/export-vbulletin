0104b68d6a7dd85e6475226f9b020b97c22a211f
David Kitchen <david@buro9.com>
2014-06-24 12:08:24 +0100

    Identified the banned roles using the bitflags

--- export/roles.go
+++ export/roles.go
@@ -70,7 +70,10 @@ func exportRole(id int64) error {
 		return nil
 	}
 
-	var publicGroup int64
+	var (
+		publicGroup    int64
+		genericOptions int64
+	)
 	vb := vbUserGroup{}
 	err := db.QueryRow(`
 SELECT usergroupid
@@ -78,6 +81,7 @@ SELECT usergroupid
       ,description
       ,forumpermissions
       ,ispublicgroup
+      ,genericoptions
   FROM `+config.DB.TablePrefix+`usergroup
  WHERE usergroupid = ?`,
 		id,
@@ -87,6 +91,7 @@ SELECT usergroupid
 		&vb.Description,
 		&vb.ForumPermissions,
 		&publicGroup,
+		&genericOptions,
 	)
 	if err != nil {
 		return err
@@ -96,39 +101,50 @@ SELECT usergroupid
 	ex.ID = vb.UserGroupID
 	ex.Name = vb.Title
 	ex.Text = vb.Description
-	ex.Banned = (vb.ForumPermissions == 0)
 
 	if publicGroup == 0 {
 		ex.DefaultRole = true
 	}
 
 	// From vBulletin includes/xml/bitfield_vbulletin.xml
+	// <group name="genericoptions">
+	//  <bitfield name="showgroup"         >1</bitfield>
+	//  <bitfield name="showbirthday"      >2</bitfield>
+	//  <bitfield name="showmemberlist"    >4</bitfield>
+	//  <bitfield name="showeditedby"      >8</bitfield>
+	//  <bitfield name="allowmembergroups" >16</bitfield>
+	//  <bitfield name="isnotbannedgroup"  >32</bitfield>
+	//  <bitfield name="requirehvcheck"    >64</bitfield>
+	// </group>
+	ex.Banned = genericOptions&32 == 0
+
+	// From vBulletin includes/xml/bitfield_vbulletin.xml
 	// <group name="forumpermissions">
-	// 	<bitfield name="canview"               group="forum_viewing_permissions"  >1</bitfield>
-	// 	<bitfield name="canviewthreads"        group="forum_viewing_permissions"  >524288</bitfield>
-	// 	<bitfield name="canviewothers"         group="forum_viewing_permissions"  >2</bitfield>
-	// 	<bitfield name="cansearch"             group="forum_searching_permissions">4</bitfield>
-	// 	<bitfield name="canemail"              group="forum_viewing_permissions"  >8</bitfield>
-	// 	<bitfield name="canpostnew"            group="post_thread_permissions"    >16</bitfield>
-	// 	<bitfield name="canreplyown"           group="post_thread_permissions"    >32</bitfield>
-	// 	<bitfield name="canreplyothers"        group="post_thread_permissions"    >64</bitfield>
-	// 	<bitfield name="caneditpost"           group="post_thread_permissions"    >128</bitfield>
-	// 	<bitfield name="candeletepost"         group="post_thread_permissions"    >256</bitfield>
-	// 	<bitfield name="candeletethread"       group="post_thread_permissions"    >512</bitfield>
-	// 	<bitfield name="canopenclose"          group="post_thread_permissions"    >1024</bitfield>
-	// 	<bitfield name="canmove"               group="post_thread_permissions"    >2048</bitfield>
-	// 	<bitfield name="cangetattachment"      group="forum_viewing_permissions"  >4096</bitfield>
-	// 	<bitfield name="canpostattachment"     group="attachment_permissions"     >8192</bitfield>
-	// 	<bitfield name="attachlimit"           group="attachment_permissions"     >1</bitfield>
-	// 	<bitfield name="canpostpoll"           group="poll_permissions"           >16384</bitfield>
-	// 	<bitfield name="canvote"               group="poll_permissions"           >32768</bitfield>
-	// 	<bitfield name="canthreadrate"         group="post_thread_permissions"    >65536</bitfield>
-	// 	<bitfield name="followforummoderation" group="post_thread_permissions"    >131072</bitfield>
-	// 	<bitfield name="canseedelnotice"       group="forum_viewing_permissions"  >262144</bitfield>
-	// 	<bitfield name="cantagown"             group="post_thread_permissions"    >1048576</bitfield>
-	// 	<bitfield name="cantagothers"          group="post_thread_permissions"    >2097152</bitfield>
-	// 	<bitfield name="candeletetagown"       group="post_thread_permissions"    >4194304</bitfield>
-	// 	<bitfield name="canseethumbnails"      group="forum_viewing_permissions"  >8388608</bitfield>
+	// 	<bitfield name="canview"               >1</bitfield>
+	// 	<bitfield name="canviewthreads"        >524288</bitfield>
+	// 	<bitfield name="canviewothers"         >2</bitfield>
+	// 	<bitfield name="cansearch"             >4</bitfield>
+	// 	<bitfield name="canemail"              >8</bitfield>
+	// 	<bitfield name="canpostnew"            >16</bitfield>
+	// 	<bitfield name="canreplyown"           >32</bitfield>
+	// 	<bitfield name="canreplyothers"        >64</bitfield>
+	// 	<bitfield name="caneditpost"           >128</bitfield>
+	// 	<bitfield name="candeletepost"         >256</bitfield>
+	// 	<bitfield name="candeletethread"       >512</bitfield>
+	// 	<bitfield name="canopenclose"          >1024</bitfield>
+	// 	<bitfield name="canmove"               >2048</bitfield>
+	// 	<bitfield name="cangetattachment"      >4096</bitfield>
+	// 	<bitfield name="canpostattachment"     >8192</bitfield>
+	// 	<bitfield name="attachlimit"           >1</bitfield>
+	// 	<bitfield name="canpostpoll"           >16384</bitfield>
+	// 	<bitfield name="canvote"               >32768</bitfield>
+	// 	<bitfield name="canthreadrate"         >65536</bitfield>
+	// 	<bitfield name="followforummoderation" >131072</bitfield>
+	// 	<bitfield name="canseedelnotice"       >262144</bitfield>
+	// 	<bitfield name="cantagown"             >1048576</bitfield>
+	// 	<bitfield name="cantagothers"          >2097152</bitfield>
+	// 	<bitfield name="candeletetagown"       >4194304</bitfield>
+	// 	<bitfield name="canseethumbnails"      >8388608</bitfield>
 	// </group>
 	perms := f.ForumPermissions{}
 	perms.View = vb.ForumPermissions&1 != 0
@@ -159,8 +175,6 @@ SELECT usergroupid
 		return nil
 	case 5, 6, 7:
 		ex.Moderator = true
-	case 8:
-		ex.Banned = true
 	}
 
 	rows, err := db.Query(`
