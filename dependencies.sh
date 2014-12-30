dc241381c31ea936ba39bdeef7d9a036781d199c
David Kitchen <david@buro9.com>
2014-12-30 21:35:21 +0000

    Removed dependency that may be manually installed

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,4 @@
 go get -u github.com/cheggaaa/pb
 go get -u github.com/go-sql-driver/mysql
-go get -u github.com/microcosm-cc/export-schemas/go/forum
+#go get -u github.com/microcosm-cc/export-schemas/go/forum
 go get -u github.com/BurntSushi/toml
